15249b4aec9d4a67937ffcfb8ec6751290941c7a
Boris Grozev <boris@jitsi.org>
2024-11-04 18:07:24 -0600

    Remove unused test files.

--- src/index.js
+++ src/index.js
@@ -6,10 +6,4 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(
-
-    // <React.StrictMode>
-        <App />
-
-    // </React.StrictMode>
-);
+root.render(<App />);
