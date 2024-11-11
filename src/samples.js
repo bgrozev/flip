ec8bdf03c24ac460a0b4087f7cc09379038cf429
Boris Grozev <boris@jitsi.org>
2024-11-10 18:41:30 -0600

    feat: Default to a standard pattern, adjust samples.

--- src/samples.js
+++ src/samples.js
@@ -1,24 +1,19 @@
 import sample450Corrected from './sample-corrected.csv';
-import sampleStraightInReduced from './sample-straight-in-reduced.csv';
-import sampleStraightIn from './sample-straight-in.csv';
+import samplePattern from './sample-pattern.csv';
 import sample450 from './sample.csv';
 
 export const SAMPLES = [
     {
-        name: '[1] Sample 450',
+        name: '[1] 900-600-300 ft, 2.6 glide, 10 mph descent',
+        sample: samplePattern
+    },
+    {
+        name: '[2] Sample 450',
         sample: sample450
     },
     {
-        name: '[2] Sample 450, corrected',
+        name: '[3] Sample 450, corrected',
         sample: sample450Corrected
-    },
-    {
-        name: '[3] Sample straight-in',
-        sample: sampleStraightIn
-    },
-    {
-        name: '[4] Sample straight-in, reduced',
-        sample: sampleStraightInReduced
     }
 ];
 
